c66d3a3d0cafa5326d1c303fa47e691a0a2c319a
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2019-09-24 18:53:30 +0530

    Add files via upload

---
+++ java solution/rotate.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Rotate{
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+		
+		int testcase = sc.nextInt();
+		
+		while(testcase-- > 0){
+            int n = sc.nextInt();
+		    int a[] = new int[n];
+		    int d = sc.nextInt();
+		    for(int i = 0; i<n ;i++){
+		        a[i] = sc.nextInt();
+            }
+		    rotateArray(a, n, d);
+		}
+    }
+
+    public static void rotateArray(int[] arr, int n, int d){
+        int[] arr2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr2[i] = arr[d%n]; 
+            d++;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr2[i] + " ");            
+        }
+    }
+
+}

efeb39efa71233d7e67add02496b3011084de356
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2019-09-29 11:07:23 +0530

    Add files via upload

---
+++ java solution/inorder.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+// A Binary Tree node
+class Node{
+    int data;
+    Node left, right;
+    Node(int item){
+        data = item;
+        left = right = null;
+    }
+}
+
+class Main{
+    
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0){
+            HashMap<Integer, Node> m = new HashMap<Integer, Node> ();
+            int n = sc.nextInt();
+            Node root = null;
+            while (n > 0){
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                char lr = sc.next().charAt(0);
+                Node parent = m.get(n1);
+                if (parent == null){
+                    parent = new Node(n1);
+                    m.put(n1, parent);
+                    if (root == null)
+                        root = parent;
+                }
+                Node child = new Node(n2);
+                if (lr == 'L')
+                    parent.left = child;
+                else
+                    parent.right = child;
+                m.put(n2, child);
+                n--;
+            }
+            Tree g = new Tree();
+            g.inOrder(root);
+            System.out.print(" ");
+            t--;
+        }
+    }
+}
+
+class Tree{
+    void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+}

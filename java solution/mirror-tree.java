5b58b11392e66e80ad34a0847dfca8d3b142290c
Mustafiz Kaifee <49086821+Mustafiz04@users.noreply.github.com>
2019-09-29 11:41:44 +0530

    Add files via upload

---
+++ java solution/mirror-tree.java
@@ -0,0 +1,116 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node left, right;
+    Node(int item){
+        data = item;
+        left = right = null;
+    }
+}
+
+class Mirror_Tree{
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+
+        inOrder(node.right);
+    }
+    
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        Mirror_Tree mt = new Mirror_Tree();
+        int t = sc.nextInt();
+        while (t > 0)
+        {
+            HashMap<Integer, Node> m = new HashMap<Integer, Node> ();
+            int n = sc.nextInt();
+            Node root = null;
+            while (n > 0)
+            {
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                char lr = sc.next().charAt(0);
+
+                Node parent = m.get(n1);
+                if (parent == null)
+                {
+                    parent = new Node(n1);
+                    m.put(n1, parent);
+                    if (root == null)
+                        root = parent;
+                }
+                Node child = new Node(n2);
+                if (lr == 'L')
+                    parent.left = child;
+                else
+                    parent.right = child;
+                m.put(n2, child);
+                n--;
+            }
+            Tree g = new Tree();
+            g.mirror(root);
+            mt.inOrder(root);
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+
+// recursion
+class Tree{
+    Node mirror(Node node){
+        if(node == null){
+            return node;
+        }
+        
+        Node left = mirror(node.left);
+        Node right = mirror(node.right);
+        
+        node.left = right;
+        node.right = left;
+        
+        return node; 
+    }
+}
+
+
+// iteration
+class Tree{
+    void mirror(Node node){
+        if(node == null){
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+
+        while(q.size() > 0){
+
+            Node c = q.peek();
+            q.remove();
+
+            Node temp = c.left;
+            c.left = c.right;
+            c.right = temp;
+
+            if(c.left != null){
+                q.add(c.left);
+            }
+            if(c.right != null){
+                q.add(c.right);
+            }
+
+        }
+
+
+    }
+}
